97e4a635e540148b392a52b713a8b74c61eafc7e
Gonzalo Mahserdjian <gsmx64@outlook.com>
2024-06-27 19:40:08 -0300

    Fix: Little change for render.com CORS

--- backend/src/constants/cors.ts
+++ backend/src/constants/cors.ts
@@ -6,7 +6,7 @@ import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.int
 export const CORS: CorsOptions = {
   origin: (process.env.NODE_ENV === 'production') ? process.env.APP_CORS_PROD_WHITELIST.split(", ") : true,
   credentials: true,
-  allowedHeaders: 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, access_token',
+  allowedHeaders: 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, Access-Control-Allow-Origin, access_token',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   preflightContinue: false,
   optionsSuccessStatus: 204
